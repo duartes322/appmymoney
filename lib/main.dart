2c59f6ea00a237512f0224ea90820abf364f18f6
Felipe Macedo <duartes.main@gmail.com>
2023-09-10 15:27:56 -0300

    Adicionadas paginas de cadastro e dio

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:app_my_money/src/modules/initial/page/initial_page.dart';
 import 'package:app_my_money/src/modules/login/page/login_page.dart';
+import 'package:app_my_money/src/modules/personal_register/page/personal_register.dart';
+import 'package:app_my_money/src/modules/register/page/register_page.dart';
 import 'package:app_my_money/src/router/app_router.dart';
 import 'package:flutter/material.dart';
 
@@ -23,6 +25,8 @@ class App extends StatelessWidget {
       darkTheme: ThemeData.dark(),
       routes: {
         AppRouter.login: (context) => const LoginPage(),
+        AppRouter.register: (context) => const RegisterPage(),
+        AppRouter.personalRegister: (context) => const PersonalRegisterPage(),
       },
     );
   }
